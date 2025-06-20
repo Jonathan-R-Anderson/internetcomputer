f8fce7e19df977745bf689e940a36ba700944603
Jonathan-R-Anderson <77250765+Jonathan-R-Anderson@users.noreply.github.com>
2025-06-19 20:28:20 -0400

    Add tss_flush implementation

--- arch/x86/cpu/tss.s
+++ arch/x86/cpu/tss.s
@@ -1,16 +1,25 @@
-# tss.s (AT&T syntax, 64-bit)
-# Provides a routine to load the Task Register with the
-# selector for our TSS descriptor in the GDT.
+# tss.s - Task State Segment helpers (AT&T syntax, 64-bit)
 
 .section .text
 .code64
+
+## tss_flush()
+## Loads the task register with our TSS selector (assumes selector 0x28).
+.global tss_flush
+.type tss_flush, @function
+tss_flush:
+    movw $0x28, %ax
+    ltr %ax
+    retq
+.size tss_flush, .-tss_flush
+
+## load_tss(selector)
+## Load the task register with the selector passed in DI.
 .global load_tss
 .type load_tss, @function
-
 load_tss:
-    # x86-64 System V ABI: first argument is in %di
     movw %di, %ax
-    ltr %ax            # Load the Task Register with the selector
+    ltr %ax
     retq
 
 .section .note.GNU-stack, "", @progbits
