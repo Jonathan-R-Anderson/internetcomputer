335fcc5d99357147754c235e06614a355f1ef3f9
Jonathan Anderson <jranderson404@gmail.com>
2025-06-23 16:54:14 -0400

    debugging shelly

---
+++ dependencies/ttyShelly/test/examples/tty-shell.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExtendedDefaultRules #-}
+import Shelly
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Control.Monad (unless)
+import System.IO (hFlush, stdout, stdin, BufferMode(..), hSetBuffering, isEOF)
+
+default (T.Text)
+
+main :: IO ()
+main = shellyNoDir $ do
+  liftIO $ do
+    hSetBuffering stdout LineBuffering
+    hSetBuffering stdin LineBuffering
+  loop
+  where
+    loop :: Sh ()
+    loop = do
+      liftIO $ putStr "ttyShelly> "
+      liftIO $ hFlush stdout
+      eof <- liftIO isEOF
+      unless eof $ do
+        line <- liftIO TIO.getLine
+        unless (T.strip line == "exit") $ do
+          verbosely $ run_ "bash" ["-c", line]
+          loop
+
