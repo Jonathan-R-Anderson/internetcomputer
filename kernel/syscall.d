4cb4eba90479dffa6a27e1270ae090faa17621a8
Jonathan-R-Anderson <77250765+Jonathan-R-Anderson@users.noreply.github.com>
2025-06-26 01:27:04 -0400

    Add Plan9-style open syscall

--- kernel/syscall.d
+++ kernel/syscall.d
@@ -3,7 +3,7 @@ module kernel.syscall;
 pragma(LDC_no_moduleinfo);
 
 import kernel.terminal : terminal_putchar, terminal_writestring;
-import kernel.fs : fs_create_user;
+import kernel.fs : fs_create_user, fs_open_file;
 import kernel.types; // for ulong
 
 public:
@@ -11,6 +11,7 @@ public:
 enum SyscallID : ulong {
     WriteString = 0,
     CreateUser  = 1,
+    Open        = 2,
 }
 
 alias SyscallHandler = extern(C) long function(ulong, ulong, ulong, ulong, ulong, ulong);
@@ -33,6 +34,13 @@ extern(C) long sys_create_user(ulong namePtr, ulong, ulong, ulong, ulong, ulong)
     return 0;
 }
 
+extern(C) long sys_open(ulong pathPtr, ulong mode, ulong, ulong, ulong, ulong)
+{
+    auto path = cast(const(char)*)pathPtr;
+    int fd = fs_open_file(path, cast(int)mode);
+    return fd;
+}
+
 extern(C) long do_syscall(ulong id, ulong a1, ulong a2, ulong a3, ulong a4, ulong a5, ulong a6)
 {
     if(id < g_syscalls.length && g_syscalls[id] !is null)
@@ -46,4 +54,5 @@ extern(C) void syscall_init()
         g_syscalls[i] = null;
     g_syscalls[cast(size_t)SyscallID.WriteString] = &sys_write_string;
     g_syscalls[cast(size_t)SyscallID.CreateUser]  = &sys_create_user;
+    g_syscalls[cast(size_t)SyscallID.Open]        = &sys_open;
 }
