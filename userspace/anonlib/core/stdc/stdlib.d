58742878b094c52a05bf09197e16ff79b6493f34
Joe Dirt <host@aol.com>
2025-06-30 10:27:25 -0600

    extend stdlib with strtol family and system

--- userspace/anonlib/core/stdc/stdlib.d
+++ userspace/anonlib/core/stdc/stdlib.d
@@ -8,4 +8,8 @@ void free(void* p){}
 void* calloc(size_t n, size_t s){ return null; }
 void* realloc(void* p, size_t n){ return null; }
 int atoi(const char* s){ return 0; }
-void exit(int status) { while(true){} } 
+void exit(int status) { while(true){} }
+long strtol(const char* nptr, char** end, int base){ return 0; }
+long strtoll(const char* nptr, char** end, int base){ return 0; }
+double strtod(const char* nptr, char** end){ return 0.0; }
+int system(const char* cmd){ return 0; } 
