1264247b3b4cdb99e4b42577b983fe77292fda4f
Jonathan Anderson <jonathan.r.anderson@protonmail.com>
2025-06-29 23:50:13 -0400

    debugging

--- modules/microkernel/kernel/keyboard.d
+++ modules/microkernel/kernel/keyboard.d
@@ -115,7 +115,6 @@ extern (C) void keyboard_interrupt_handler(ubyte scancode) {
             // terminal_putchar(c); ← comment this out
         }
     }
-    log_message("keyboard_interrupt_handler triggered\n");  // Add this
 
 }
 
